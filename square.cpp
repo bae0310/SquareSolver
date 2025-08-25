774d34c5e7e4dfde7ad6ea7dda01805e84944c7e
YaroslavDrob <legionerov0310@gmail.com>
2025-08-25 14:10:59 +0300

    Structs, loop, NaN fix
    - NaN issue if fixed: error message displaces correctly
    - Solver function is returned to main file
    - Struct and loop are being used in Test function now

--- square.cpp
+++ square.cpp
@@ -6,32 +6,91 @@
 
 int main()
 {
-    double a = 0, b = 0, c = 0;
+    const double ctrl_value = 7813e+16;
+    double a = 0, b = 0, c = ctrl_value;
+
+    AllTests();
+
     printf("Enter a, b, c: ");
     scanf("%lg %lg %lg", &a, &b, &c);
-
+    printf("a = %lg\nb = %lg\nc = %lg\n", a, b, c);
 
     double x1 = 0, x2 = 0;
-    int SquareSolve_Resp = SquareSolve(a, b, c, &x1, &x2);
-    Count_Failed();
+    int Rts = SquareSolve(a, b, c, &x1, &x2);
 
-    switch (SquareSolve_Resp)
+
+    switch (Rts)
     {
     case 0: printf("No roots, D < 0\n");
             break;
-    case 1: printf("Number of roots: %d\n", SquareSolve_Resp);
+    case 1: printf("Number of roots: %d\n", Rts);
             printf("Root: x = %lg\n", x1);
             break;
-    case 2: printf("Number of roots: %d\n", SquareSolve_Resp);
+    case 2: printf("Number of roots: %d\n", Rts);
             printf("Roots: x1 = %lg, x2 = %lg\n", x1, x2);
             break;
     case -1: printf("Roots: Infinite number of roots\n");
              break;
     case -2: printf("No roots\n");
              break;
-    default: printf ("main(): ERROR: SquareSolve_Resp = %d\n", SquareSolve_Resp);
+    default: printf ("main(): ERROR: Rts = %d, INCORRECT INPUT\n", Rts);
              return 1;
     }
     return 0;
 }
+//----------------------------------------------------
+//! Solver for a square equation ax^2 + bx + c = 0
+//!
+//! @param [in]    a    a-coefficient
+//! @param [in]    b    b-coefficient
+//! @param [in]    c    c-coefficient
+//! @param [out]   x1   pointer for 1st root
+//! @param [out]   x2   pointer for 2nd root
+//!
+//! @return Number of roots and the exact roots if they are exist
+//!
+//----------------------------------------------------
+double SquareSolve(double a, double b, double c,
+                   double* x1, double* x2)
+{
+    const double ctrl_value = 7813e+16;
 
+    assert(x1 != NULL);
+    assert(x2 != NULL);
+    assert(x1 != x2);
+
+    double d = b*b - 4*a*c, sqrt_d = sqrt(d); // todo initialize - Done // todo assert
+    if (c == ctrl_value) {
+            return -3;
+    }
+    if (d < 0) {
+        return 0;
+    } else {
+        if (a == 0) {
+            if (b == 0) {
+                if (c == 0) {
+                return -1;
+            } else {
+            return -2;
+            }} else {
+            *x1 = *x2 = (-c / b);
+            return 1;
+        }} else {
+            if (sqrt_d == 0) {
+                if (b == 0) {
+                    *x1 = *x2 = 0;
+                    return 1;
+                    } else {
+                *x1 = *x2 = (-b / (2*a));
+                return 1;
+            }} else {
+                *x1 = ((-b + sqrt_d) / (2*a));
+                *x2 = ((-b - sqrt_d) / (2*a));
+                if (*x1 > *x2) {
+                    Swap(x1, x2);
+                }
+                return 2;
+            }
+        }
+    }
+}
--- tester.cpp
+++ tester.cpp
@@ -2,91 +2,48 @@
 #include <math.h>
 #include "functions.cpp"
 
-//----------------------------------------------------
-//! Solver for a square equation ax^2 + bx + c = 0
-//!
-//! @param [in]    a    a-coefficient
-//! @param [in]    b    b-coefficient
-//! @param [in]    c    c-coefficient
-//! @param [out]   x1   pointer for 1st root
-//! @param [out]   x2   pointer for 2nd root
-//!
-//! @return Number of roots and the exact roots if they are exist
-//!
-//----------------------------------------------------
 double SquareSolve(double a, double b, double c,
-                   double* x1, double* x2)
+                   double* x1, double* x2);
+
+struct TestData
 {
+    double a, b, c;
+    double Rts;
+    double x1, x2;
+};
 
-    assert(std::isfinite (a));
-    assert(std::isfinite (b));
-    assert(std::isfinite (c));
-
-    assert(x1 != NULL);
-    assert(x2 != NULL);
-    assert(x1 != x2);
-
-    double d = b*b - 4*a*c, sqrt_d = sqrt(d); // todo initialize - Done // todo assert
-    if (d < 0) {
-        return 0;
-    } else {
-        if (a == 0) {
-            if (b == 0) {
-                if (c == 0) {
-                return -1;
-            } else {
-            return -2;
-            }} else {
-            *x1 = *x2 = (-c / b);
-            return 1;
-        }} else {
-            if (sqrt_d == 0) {
-                if (b == 0) {
-                    *x1 = *x2 = 0;
-                    return 1;
-                    } else {
-                *x1 = *x2 = (-b / (2*a));
-                return 1;
-            }} else {
-                *x1 = ((-b + sqrt_d) / (2*a));
-                *x2 = ((-b - sqrt_d) / (2*a));
-                if (*x1 > *x2) {
-                    Swap(x1, x2);
-                }
-                return 2;
-            }
-        }
-    }
-}
-
-int OneTest(double a, double b, double c, double SquareSolve_Resp,
-            double x1_ref, double x2_ref)
+int OneTest(TestData test)
 {
     double x1 = 0, x2 = 0;
-    int Rts = SquareSolve(a, b, c, &x1, &x2);
-    if (!(Equation(x1, x1_ref) && Equation(x2, x2_ref)) && Rts == SquareSolve_Resp) {
+    int Rts = SquareSolve(test.a, test.b, test.c, &x1, &x2);
+    if (!(Equation(x1, test.x1) && Equation(x2, test.x2)) && test.Rts == Rts) {
         return 1;
     } else {
         return 0;
     }
 }
-int Count_Failed()
+
+int AllTests()
 {
     int failed = 0;
-    failed += OneTest(1, 5, 6, 2, -3, -2);
-    failed += OneTest(1, 4, 4, 1, -2, -2);
-    failed += OneTest(3, 0, 0, 1, 0, 0);
-    failed += OneTest(0, 4, 0, 1, -0, -0);
-    failed += OneTest(0, 0, 0, -1, 0, 0);
+    TestData global_test[6] = { {.a = 1, .b = 5, .c = 6, .Rts = 2, .x1 = -3, .x2 = -2},
+                                {.a = 1, .b = 4, .c = 4, .Rts = 1, .x1 = -2, .x2 = -2},
+                                {.a = 3, .b = 0, .c = 0, .Rts = 1, .x1 = 0, .x2 = 0},
+                                {.a = 0, .b = 4, .c = 0, .Rts = 1, .x1 = -0, .x2 = -0},
+                                {.a = 0, .b = 0, .c = 0, .Rts = -1, .x1 = 0, .x2 = 0},
+                                {.a = 0, .b = 0, .c = 3, .Rts = 0, .x1 = 0, .x2 = 0} };
+    int counter = sizeof(global_test) / sizeof(global_test[0]);
+    for (int i = 0; i < counter; i++) {
+        failed += OneTest(global_test[i]);
+    }
     printf("FAILED ATTEMPTS: %d  ---> ", failed);
     if (failed == 0) {
-        printf("SUCCESS\n");
+        printf("CORRECT FORMULA\n");
     } else {
         printf("ERROR\n");
     }
     return failed;
 }
-
 
 
 
